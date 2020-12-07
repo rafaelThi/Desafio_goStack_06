c638d1ab21ba8e9a13647b43b26a5dbb2774e14a
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2020-12-07 16:54:09 -0300

    Criando Migrations/Corrigindo bug do ESlint

---
+++ src/database/migrations/1607369667910-AddCategoryIdToTrasactions.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+// TableColumn para criar uma coluna de uma tabela
+// TableForeignKey parareferenciar uma coluna a uma tabela
+export default class AddCategoryIdToTrasactions1607369667910
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'TransactionCategory',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+    await queryRunner.dropColumn('transactions', 'category_id'),
+  }
+}

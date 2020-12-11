91957c1a3c812ff0a7cd1d5cac629ec8de1377c2
Rafael Thiago Pereira de Melo <rafael.pereira20@icloud.com>
2020-12-11 10:44:28 -0300

    Lendo, filtrando e cadastrando categorias de um arquino CSV

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,19 +1,34 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
+
 import csvParse from 'csv-parse';
 import fs from 'fs';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CVSTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
   async execute(filepath: string): Promise<Transaction[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
     const contactsReadStream = fs.createReadStream(filepath);
+
     const parsers = csvParse({
       from_line: 2, // para não pegar a 1° linha.
     });
 
     const parseCSV = contactsReadStream.pipe(parsers);
 
-    const transactions = [];
-    const categories = [];
+    const transactions: CVSTransaction[] = [];
+    const categories: string[] = [];
 
     parseCSV.on('data', async line => {
       const [title, type, value, category] = line.map((cell: string) => {
@@ -27,7 +42,28 @@ class ImportTransactionsService {
     });
     await new Promise(resolve => parseCSV.on('end', resolve));
 
-    return { categories, transactions };
+    const existentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existentCategoriesTitle = existentCategories.map(
+      (category: Category) => {
+        return category.title;
+      },
+    );
+
+    const addCategoryTitle = categories
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCatergories = categoriesRepository.create(
+      addCategoryTitle.map(title => ({
+        title,
+      })),
+    );
+    await categoriesRepository.save(newCatergories);
   }
 }
 
